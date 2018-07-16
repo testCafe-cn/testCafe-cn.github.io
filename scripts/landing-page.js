b228dc51bd0aee74748c5fa1a21b6f0b418faf16
flyDreame <flydreame@foxmail.com>
2018-07-16 23:19:02 +0800

    初始化网站，引入testCafe官网网站资源

---
+++ scripts/landing-page.js
@@ -0,0 +1,41 @@
+---
+---
+$(function(){
+    {% if jekyll.environment == "production" %}
+    
+    var GET_STARTED_CLICKED_FLAG = 'get-started-clicked';
+
+    $('.get-started-button').on('click', function () {
+        if(!window.localStorage.getItem(GET_STARTED_CLICKED_FLAG)) {
+            ga('send', 'event', 'landingPage', 'getStartedClicked');
+            window.localStorage.setItem(GET_STARTED_CLICKED_FLAG, true);
+        }
+    });
+
+    {% endif %}
+
+    function closePlayer(e) {
+        var clickOutsidePlayer = player !== e.target && !window.has(player, e.target);
+    
+        if(clickOutsidePlayer && veil.className.indexOf('veil-on') !== -1) {
+            var iframe    = document.querySelector('#yt-iframe')
+            var iframeSrc = iframe.src;
+    
+            // NOTE: Reassigning iframe's src is a quick way to stop the video
+            // without sending postMessage.
+            iframe.src = iframeSrc;
+            veil.className = 'veil-off';
+        }
+    }
+    
+    var videoButton = document.querySelector('.video-button');
+    var veil        = document.querySelector('.veil-off');
+    var player      = document.querySelector('.player');
+    
+    videoButton.addEventListener('click', function(e) {
+        veil.className = 'veil-on';
+    });
+    
+    window.addEventListener('mousedown', closePlayer);        
+    window.addEventListener('touchstart', closePlayer);
+})
